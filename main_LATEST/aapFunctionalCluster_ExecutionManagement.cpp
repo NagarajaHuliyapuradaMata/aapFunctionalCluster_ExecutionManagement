104459462fd8f874aaba070cd56a8f67c69928f6
Nagaraja HULIYAPURADA MATA <Nagaraja.Huliyapurada-Mata@outlook.com>
2024-12-22 17:24:31 +0530

    Update: 202412241032AM

--- main_LATEST/aapFunctionalCluster_ExecutionManagement.cpp
+++ main_LATEST/aapFunctionalCluster_ExecutionManagement.cpp
@@ -63,18 +63,95 @@ Result aapFunctionalCluster_ExecutionManagement::ReportExecutionState(
    return 0;
 }
 
+/******************************************************************************/
+/* Debug code - START                                                         */
+/******************************************************************************/
+#include "infClientSwcApplDigitalTwinMotorPMSM.hpp"
+#include "ProjectSystem.hpp"
+double t = 0;
+Type_infClientSwcApplDigitalTwinMotorPMSM_stInputs  SwcApplDigitalTwinMotorPMSM_stInputs;
+Type_infClientSwcApplDigitalTwinMotorPMSM_stOutputs SwcApplDigitalTwinMotorPMSM_stOutputs;
+
+#include <iostream>
+void LogInit(void){
+   std::cout << "t"
+      << "\t" << "Tm"
+      << "\t" << "Va"
+      << "\t" << "Vb"
+      << "\t" << "Vc"
+      << "\t" << "Ia"
+      << "\t" << "Ib"
+      << "\t" << "Ic"
+      << std::endl;
+}
+void LogUpdate(void){
+   std::cout << t
+      << "\t" << SwcApplDigitalTwinMotorPMSM_stInputs.Tm
+      << "\t" << SwcApplDigitalTwinMotorPMSM_stInputs.Va
+      << "\t" << SwcApplDigitalTwinMotorPMSM_stInputs.Vb
+      << "\t" << SwcApplDigitalTwinMotorPMSM_stInputs.Vc
+      << "\t" << SwcApplDigitalTwinMotorPMSM_stOutputs.Ia
+      << "\t" << SwcApplDigitalTwinMotorPMSM_stOutputs.Ib
+      << "\t" << SwcApplDigitalTwinMotorPMSM_stOutputs.Ic
+      << std::endl;
+}
+/******************************************************************************/
+/* Debug code - END                                                           */
+/******************************************************************************/
+typedef enum{
+      SwcServiceSchm_eMode_OFFLINE = 0
+   ,  SwcServiceSchm_eMode_ONLINE
+}Type_SwcServiceSchm_eMode;
+
+#include <cmath>
 int main(
-       int   argc
-   ,   char* argv[]
+      int   argc
+   ,  char* argv[]
 ){
+   Type_SwcServiceSchm_eMode leMode;
+   if(1 < argc){
+      leMode = SwcServiceSchm_eMode_OFFLINE;
+   }
+   else{
+      leMode = SwcServiceSchm_eMode_ONLINE;
+   }
+
    SwcServiceEcuM.vInitFunction();
-   cpstinfClientSwcServiceEthTp->vInitFunction();
-   cpstinfClientSwcServiceDcm->vInitFunction();
+   if(SwcServiceSchm_eMode_OFFLINE == leMode){
+      LogInit();
+      pinfClientSwcApplDigitalTwinMotorPMSM->InitFunction(
+            &SwcApplDigitalTwinMotorPMSM_stInputs
+         ,  &SwcApplDigitalTwinMotorPMSM_stOutputs
+      );
+   }
+   else{
+      cpstinfClientSwcServiceEthTp->vInitFunction();
+      cpstinfClientSwcServiceDcm->vInitFunction();
+   }
+
    do{
-      cpstinfClientSwcServiceEthTp->vMainFunction();
-      cpstinfClientSwcServiceDcm->vMainFunction();
-   }while(FALSE == SwcServiceEcuM.bGetRequestShutdown());
-   cpstinfClientSwcServiceEthTp->vDeInitFunction();
+      if(SwcServiceSchm_eMode_OFFLINE == leMode){
+         SwcApplDigitalTwinMotorPMSM_stInputs.Tm = 1;
+         SwcApplDigitalTwinMotorPMSM_stInputs.Va = 220.0*sqrt(2.0)*sin(2*pi*50*t);
+         SwcApplDigitalTwinMotorPMSM_stInputs.Vb = 220.0*sqrt(2.0)*sin(2*pi*50*t - 2*pi/3.0);
+         SwcApplDigitalTwinMotorPMSM_stInputs.Vc = 220.0*sqrt(2.0)*sin(2*pi*50*t + 2*pi/3.0);
+         pinfClientSwcApplDigitalTwinMotorPMSM->MainFunction();
+         LogUpdate();
+         t += dt;
+      }
+      else{
+         cpstinfClientSwcServiceEthTp->vMainFunction();
+         cpstinfClientSwcServiceDcm->vMainFunction();
+      }
+   }while(
+         (FALSE == SwcServiceEcuM.bGetRequestShutdown())
+      && (0.5 > t)
+   );
+   if(SwcServiceSchm_eMode_OFFLINE == leMode){
+   }
+   else{
+      cpstinfClientSwcServiceEthTp->vDeInitFunction();
+   }
    return 0;
 }
 
