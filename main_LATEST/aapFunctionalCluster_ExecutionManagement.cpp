505eb1261a0003f1d9db403df620ed7435a8f522
Nagaraja HULIYAPURADA MATA <Nagaraja.Huliyapurada-Mata@outlook.com>
2025-01-31 21:58:30 +0530

    202501312201PM - baseline for motor control algorithm

--- main_LATEST/aapFunctionalCluster_ExecutionManagement.cpp
+++ main_LATEST/aapFunctionalCluster_ExecutionManagement.cpp
@@ -63,96 +63,152 @@ Result aapFunctionalCluster_ExecutionManagement::ReportExecutionState(
    return 0;
 }
 
-/******************************************************************************/
-/* Debug code - START                                                         */
-/******************************************************************************/
-#include "infClientSwcApplDigitalTwinMotorPMSM.hpp"
+#include "infClientSwcApplControlMotor.hpp"
 #include "ProjectSystem.hpp"
-double t = 0;
-Type_infClientSwcApplDigitalTwinMotorPMSM_stInputs  SwcApplDigitalTwinMotorPMSM_stInputs;
-Type_infClientSwcApplDigitalTwinMotorPMSM_stOutputs SwcApplDigitalTwinMotorPMSM_stOutputs;
 
 #include <iostream>
-void LogInit(void){
-   std::cout << "t"
-      << "\t" << "Tm"
-      << "\t" << "Va"
-      << "\t" << "Vb"
-      << "\t" << "Vc"
-      << "\t" << "Ia"
-      << "\t" << "Ib"
-      << "\t" << "Ic"
+#include <fstream>
+#include <string>
+#include <unistd.h>
+#include <cmath>
+
+double t = 0;
+std::string stringNameVersionRelease    = "RmainLATEST";
+std::string stringNameFileInputs        = "log_pProjectVirtualEcu_DigitalTwinMotorPMSM.csv";
+std::string stringNameFileOutputs       = "log_pProjectVirtualEcu_DigitalTwinMotorPMSM_RmainLATEST.csv";
+std::string stringNameFileConfiguration = "CfgSwcApplDigitalTwinMotorPMSM.hex";
+
+void print_usage(
+      int   argc
+   ,  char* argv[]
+){
+   std::cerr
+      << "Usage: " << argv[0]               << std::endl
+      << "\t[-i <name_file_input>]"         << std::endl
+      << "\t[-o <name_file_output>]"        << std::endl
+      << "\t[-c <name_file_configuration>]" << std::endl
       << std::endl;
 }
-void LogUpdate(void){
-   std::cout << t
-      << "\t" << SwcApplDigitalTwinMotorPMSM_stInputs.Tm
-      << "\t" << SwcApplDigitalTwinMotorPMSM_stInputs.Va
-      << "\t" << SwcApplDigitalTwinMotorPMSM_stInputs.Vb
-      << "\t" << SwcApplDigitalTwinMotorPMSM_stInputs.Vc
-      << "\t" << SwcApplDigitalTwinMotorPMSM_stOutputs.Ia
-      << "\t" << SwcApplDigitalTwinMotorPMSM_stOutputs.Ib
-      << "\t" << SwcApplDigitalTwinMotorPMSM_stOutputs.Ic
-      << std::endl;
-}
-/******************************************************************************/
-/* Debug code - END                                                           */
-/******************************************************************************/
+
 typedef enum{
       SwcServiceSchm_eMode_OFFLINE = 0
    ,  SwcServiceSchm_eMode_ONLINE
 }Type_SwcServiceSchm_eMode;
+Type_SwcServiceSchm_eMode geMode;
 
-#include <cmath>
+int process_argument_commandline(
+      int   argc
+   ,  char* argv[]
+){
+   if(1 < argc){
+      int  opt;
+      bool flag_i = false;
+      bool flag_o = false;
+      bool flag_c = false;
+      std::string argValue_i;
+      std::string argValue_o;
+      std::string argValue_c;
+
+      while(
+            -1
+         != (opt = getopt(argc, argv, "i:o:c:"))
+      ){
+         switch(opt){
+            case 'i': flag_i = true; argValue_i = optarg; break;
+            case 'o': flag_o = true; argValue_o = optarg; break;
+            case 'c': flag_c = true; argValue_c = optarg; break;
+            default:
+               print_usage(argc, argv);
+               return EXIT_FAILURE;
+         }
+      }
+
+      if(flag_i){
+         if(!argValue_i.empty()){
+            stringNameFileInputs = argValue_i;
+         }
+         else{
+            print_usage(argc, argv);
+         }
+      }
+      else{
+         std::cout << "option -i is required in OFFLINE mode!" << std::endl;
+         return EXIT_FAILURE;
+      }
+
+      if(flag_o){
+         if(!argValue_o.empty()){
+            std::cout << "Value for -o: " << argValue_o << std::endl;
+         }
+         else{
+            print_usage(argc, argv);
+         }
+      }
+      else{
+         size_t mid = stringNameFileInputs.find('.');
+         stringNameFileOutputs  = stringNameFileInputs.substr(0,mid-0);
+         stringNameFileOutputs += '_' + stringNameVersionRelease;
+         stringNameFileOutputs += stringNameFileInputs.substr(mid);
+      }
+
+      if(flag_c){
+         if(!argValue_c.empty()){
+            stringNameFileConfiguration = argValue_c;
+         }
+         else{
+            print_usage(argc, argv);
+         }
+      }
+      else{
+         std::cout << "Default configuration file name: " << stringNameFileConfiguration << std::endl;
+      }
+
+      geMode = SwcServiceSchm_eMode_OFFLINE;
+   }
+   else{
+      geMode = SwcServiceSchm_eMode_ONLINE;
+   }
+   return EXIT_SUCCESS;
+}
+
 int main(
       int   argc
    ,  char* argv[]
 ){
-   Type_SwcServiceSchm_eMode leMode;
-   if(1 < argc){
-      leMode = SwcServiceSchm_eMode_OFFLINE;
-   }
-   else{
-      leMode = SwcServiceSchm_eMode_ONLINE;
-   }
-
-   SwcServiceEcuM.vInitFunction();
-   if(SwcServiceSchm_eMode_OFFLINE == leMode){
-      LogInit();
-      pinfClientSwcApplDigitalTwinMotorPMSM->InitFunction(
-            &SwcApplDigitalTwinMotorPMSM_stInputs
-         ,  &SwcApplDigitalTwinMotorPMSM_stOutputs
-      );
-   }
-   else{
-      cpstinfClientSwcServiceEthTp->vInitFunction();
-      cpstinfClientSwcServiceDcm->vInitFunction();
-   }
-
-   do{
-      if(SwcServiceSchm_eMode_OFFLINE == leMode){
-         SwcApplDigitalTwinMotorPMSM_stInputs.Tm = 1;
-         SwcApplDigitalTwinMotorPMSM_stInputs.Va = 220.0*sqrt(2.0)*sin(2*pi*50*t);
-         SwcApplDigitalTwinMotorPMSM_stInputs.Vb = 220.0*sqrt(2.0)*sin(2*pi*50*t - 2*pi/3.0);
-         SwcApplDigitalTwinMotorPMSM_stInputs.Vc = 220.0*sqrt(2.0)*sin(2*pi*50*t + 2*pi/3.0);
-         pinfClientSwcApplDigitalTwinMotorPMSM->MainFunction();
-         LogUpdate();
-         t += dt;
+   if(EXIT_SUCCESS == process_argument_commandline(argc, argv)){
+      SwcServiceEcuM.vInitFunction();
+      if(SwcServiceSchm_eMode_OFFLINE == geMode){
+         pinfClientSwcApplControlMotor->InitFunction();
       }
       else{
-         cpstinfClientSwcServiceEthTp->vMainFunction();
-         cpstinfClientSwcServiceDcm->vMainFunction();
+         cpstinfClientSwcServiceEthTp->vInitFunction();
+         cpstinfClientSwcServiceDcm->vInitFunction();
       }
-   }while(
-         (FALSE == SwcServiceEcuM.bGetRequestShutdown())
-      && (0.5 > t)
-   );
-   if(SwcServiceSchm_eMode_OFFLINE == leMode){
+
+      do{
+         if(SwcServiceSchm_eMode_OFFLINE == geMode){
+            pinfClientSwcApplControlMotor->MainFunction();
+            t += double_dt;
+         }
+         else{
+            cpstinfClientSwcServiceEthTp->vMainFunction();
+            cpstinfClientSwcServiceDcm->vMainFunction();
+         }
+      }while(
+            (FALSE == SwcServiceEcuM.bGetRequestShutdown())
+         && (0.5 > t)
+      );
+      if(SwcServiceSchm_eMode_OFFLINE == geMode){
+         pinfClientSwcApplControlMotor->DeInitFunction();
+      }
+      else{
+         cpstinfClientSwcServiceEthTp->vDeInitFunction();
+      }
+      return EXIT_SUCCESS;
    }
    else{
-      cpstinfClientSwcServiceEthTp->vDeInitFunction();
+      return EXIT_FAILURE;
    }
-   return 0;
 }
 
 /******************************************************************************/
